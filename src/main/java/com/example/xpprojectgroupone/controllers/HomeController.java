f4fa7d1974380bffc240f051f73343965b268a6f
Museberg <rasmus@falk-jensen.dk>
2020-09-28 14:26:42 +0200

    Added basic controller, model, repo and service. Mainly to get the folder structure set up

---
+++ src/main/java/com/example/xpprojectgroupone/controllers/HomeController.java
@@ -0,0 +1,7 @@
+package com.example.xpprojectgroupone.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+}
---
+++ src/main/java/com/example/xpprojectgroupone/services/EquipmentService.java
@@ -0,0 +1,7 @@
+package com.example.xpprojectgroupone.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EquipmentService {
+}
