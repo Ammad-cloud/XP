baf75aaee053286994dd9437fa259802ece7fb5f
Museberg <Rasmus@falk-jensen.dk>
2020-09-28 14:13:10 +0200

    Initial commit

---
+++ src/main/java/com/example/xpprojectgroupone/XpProjectGroupOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.xpprojectgroupone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpProjectGroupOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XpProjectGroupOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/xpprojectgroupone/XpProjectGroupOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xpprojectgroupone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpProjectGroupOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
