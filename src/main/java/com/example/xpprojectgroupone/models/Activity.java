5b55cd3b31180c9bd771dee8c6f20061d2ad6584
ElmelundJC <Christianelmelund@hotmail.com>
2020-09-28 14:48:43 +0200

    Activity

---
+++ src/main/java/com/example/xpprojectgroupone/models/Activity.java
@@ -0,0 +1,68 @@
+package com.example.xpprojectgroupone.models;
+
+public class Activity {
+
+    int id;
+    String description;
+    int equipmentId;
+    int ageLimit;
+    int heightLimit;
+    double price;
+
+    public Activity(int id, String description, int equipmentId, int ageLimit, int heightLimit, double price) {
+        this.id = id;
+        this.description = description;
+        this.equipmentId = equipmentId;
+        this.ageLimit = ageLimit;
+        this.heightLimit = heightLimit;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getEquipmentId() {
+        return equipmentId;
+    }
+
+    public void setEquipmentId(int equipmentId) {
+        this.equipmentId = equipmentId;
+    }
+
+    public int getAgeLimit() {
+        return ageLimit;
+    }
+
+    public void setAgeLimit(int ageLimit) {
+        this.ageLimit = ageLimit;
+    }
+
+    public int getHeightLimit() {
+        return heightLimit;
+    }
+
+    public void setHeightLimit(int heightLimit) {
+        this.heightLimit = heightLimit;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
---
+++ src/main/java/com/example/xpprojectgroupone/repositories/ActivityRepo.java
@@ -0,0 +1,6 @@
+package com.example.xpprojectgroupone.repositories;
+
+public class ActivityRepo {
+
+
+}
