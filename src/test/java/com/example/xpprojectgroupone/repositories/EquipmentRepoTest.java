0d62cf6b16ac8f6b5e1346f2bc805ef38d99a24e
Julius Stenbæk Christoffersen <julius@adnudging.com>
2020-09-30 14:49:14 +0200

    Test and pom changes

---
+++ src/test/java/com/example/xpprojectgroupone/repositories/EquipmentRepoTest.java
@@ -0,0 +1,17 @@
+package com.example.xpprojectgroupone.repositories;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EquipmentRepoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void add() {
+    }
+}
