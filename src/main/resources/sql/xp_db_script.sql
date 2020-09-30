60bcc2c9bbd535032fc1a8d80fb3860cea214243
Julius Stenbæk Christoffersen <julius@adnudging.com>
2020-09-30 14:25:25 +0200

    Added db initialization

--- src/main/resources/sql/xp_db_script.sql
+++ src/main/resources/sql/xp_db_script.sql
@@ -1,3 +1,7 @@
+DROP SCHEMA IF EXISTS `xp-adventure-group-one` ;
+CREATE SCHEMA IF NOT EXISTS `xp-adventure-group-one` DEFAULT CHARACTER SET utf8 ;
+USE `xp-adventure-group-one` ;
+
 create table Activity
 (
 	id int auto_increment,
