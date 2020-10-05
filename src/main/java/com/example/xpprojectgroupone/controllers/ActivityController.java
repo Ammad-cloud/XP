fa91cd0b3b0c83f3f6e3eb349c36efe97c33c920
Ammad-cloud <58288491+Ammad-cloud@users.noreply.github.com>
2020-10-05 13:22:23 +0200

    GUI frontpage

--- src/main/java/com/example/xpprojectgroupone/controllers/ActivityController.java
+++ src/main/java/com/example/xpprojectgroupone/controllers/ActivityController.java
@@ -1,4 +1,24 @@
 package com.example.xpprojectgroupone.controllers;
 
+import com.example.xpprojectgroupone.models.Activity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping(value = {"/activities"})
 public class ActivityController {
+
+    @GetMapping("/list")
+    public String index(Model model){
+        List<Activity> activities = new ArrayList<>();
+        Activity activity = new Activity(1, "Fodbold", "spark til en bold", 1, 180, 18, 300);
+        activities.add(activity);
+        model.addAttribute("activity", activities);
+        return "activities/list";
+    }
 }
